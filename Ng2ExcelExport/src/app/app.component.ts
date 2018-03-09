76b2a07e16e2cc98d9094f8d649c0b0ec0733bec
unknown <sudeendracse@gmail.com>
2018-03-09 22:53:41 +0530

    Added styling to Excel

--- Ng2ExcelExport/src/app/app.component.ts
+++ Ng2ExcelExport/src/app/app.component.ts
@@ -1,21 +1,68 @@
-import { Component } from '@angular/core';
-import { ExcelService } from './excel.service';
+import { ElementRef, OnInit, ViewChild } from '@angular/core';
 import { PERSONS, Person } from './model';
+
+import { Component } from '@angular/core';
+import {Element} from '@angular/compiler';
+import { ExcelService } from './excel.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  @ViewChild('dataTableD') tableData:Element;
   persons: Person[];
-
+  jsondata:any[]=[];
   constructor(private excelService: ExcelService) {
     this.excelService = excelService;
-    this.persons = PERSONS;
+   this.jsondata=  [
+  {
+    "AssociateName": "Aaron Gutierrez",
+    "client_name": "Carefusion",
+    "AccountOwner": "Lori Kugler",
+    "CSA": "Ariana Velazco",
+    "placement_fee": "0.00",
+    "reg_payrate": 12.5,
+    "ot_payrate": 18.75,
+    "dt_payrate": 25,
+    "reg_hours": 183
+  },
+    {
+    "AssociateName": "Aaron Gutierrez",
+    "client_name": "Carefusion",
+    "AccountOwner": "Lori Kugler",
+    "CSA": "Ariana Velazco",
+    "placement_fee": "0.00",
+    "reg_payrate": 12.5,
+    "ot_payrate": 18.75,
+    "dt_payrate": 25,
+    "reg_hours": 184
+  },
+  {
+    "Period": "2018-04-30-2018-05-27"
+  }
+];
+console.log("FILTER");
+  console.log(this.jsondata.find(item => item.id === 183));
   }
 
+
+
+
+
+ngOnInit() {
+this.persons = PERSONS;
+//console.log(this);
+  
+}
+
+
+
+
+
+
   exportToExcel(event) {
-    this.excelService.exportAsExcelFile(PERSONS, 'persons');
+    this.excelService.exportAsExcelFile(this.jsondata, 'persons');
   }
 }
--- Ng2ExcelExport/src/app/excel.service.ts
+++ Ng2ExcelExport/src/app/excel.service.ts
@@ -1,5 +1,6 @@
 import * as FileSaver from 'file-saver';
 import * as XLSX from 'xlsx';
+import * as XLSXStyle from 'xlsx-style';
 
 import { Injectable } from '@angular/core';
 
@@ -8,14 +9,46 @@ const EXCEL_EXTENSION = '.xlsx';
 
 @Injectable()
 export class ExcelService {
-
+    
   constructor() { }
+ header:any[]=['A1','B1','C1','D1','E1','F1','G1','H1','I1','J1'];
 
-  public exportAsExcelFile(json: any[], excelFileName: string): void {
+   public exportAsExcelFile(json: any[], excelFileName: string): void {
+      const wscols = [
+     
+      {wpx: 150},{wpx: 150},{wpx: 150},{wpx: 150},{wpx: 150},
+      {wpx: 150},{wpx: 150},{wpx: 150},{wpx: 150},{wpx: 150},
+      {wpx: 150},{wpx: 150},{wpx: 150},{wpx: 150},{wpx: 150},
+      {wpx: 150},{wpx: 150},{wpx: 150},{wpx: 150},{wpx: 150}
+    ];
     const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
+    console.log(worksheet);
+    for(let i=0;i<this.header.length;i++){
+       this.wrapAndCenterCell(worksheet[this.header[i]]);
+       worksheet['!cols'] = wscols;
+    }
+   
     const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
-    const excelBuffer: any = XLSX.writeFile(workbook,excelFileName+'.xlsx',{ bookType: 'xlsx', type: 'array' });
-   // this.saveAsExcelFile(excelBuffer, excelFileName);
+    // Use XLSXStyle instead of XLSX write function which property writes cell styles.
+    const excelBuffer: any = XLSXStyle.write(workbook, { bookType: 'xlsx', type: 'buffer' });
+    this.saveAsExcelFile(excelBuffer, excelFileName);
+  }
+
+  private wrapAndCenterCell(cell: XLSX.CellObject) {
+    const wrapAndCenterCellStyle = {  fill: {fgColor:{rgb: "4596E2"} },
+      font:{name:'Calibri',s :'12',bold:true},border:{
+        top:{ style: 'thick', color: { rgb: "000000" } },
+      bottom:{ style: 'thick', color: { rgb: "000000" }},
+    left:{ style: 'thick', color: { rgb: "000000" } },
+  right:{ style: 'thick', color: { rgb: "000000" } }
+      }
+ };
+ console.log(wrapAndCenterCellStyle);
+    this.setCellStyle(cell, wrapAndCenterCellStyle);
+  }
+
+  private setCellStyle(cell: XLSX.CellObject, style: {}) {
+    cell.s = style;
   }
 
   private saveAsExcelFile(buffer: any, fileName: string): void {
